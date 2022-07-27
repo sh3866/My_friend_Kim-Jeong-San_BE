3c299b04ddcb30ce0b022536caa21eed728c7c2c
Hyomk <qet557@gmail.com>
2022-07-28 01:03:12 +0900

    fix : User,Room Model 수정

--- models/userRoom.js
+++ models/userRoom.js
@@ -3,7 +3,14 @@ const Sequelize = require("sequelize");
 module.exports = class UserRoom extends Sequelize.Model {
     static init(sequelize) {
         return super.init(
-            {},
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true,
+                    allowNull: false,
+                },
+            },
             {
                 sequelize,
                 timestamps: true, // create at 에 자동 설정
@@ -15,5 +22,25 @@ module.exports = class UserRoom extends Sequelize.Model {
                 collate: "utf8mb4_general_ci",
             }
         );
+    }
+    static associate(db) {
+        db.Room.belongsToMany(db.User, {
+            through: db.UserRoom,
+            paranoid: true,
+        });
+        db.User.belongsToMany(db.Room, {
+            through: db.UserRoom,
+            paranoid: true,
+        });
+        db.Room.hasMany(db.UserRoom);
+        db.UserRoom.belongsTo(db.Room, {
+            foreignKey: "RoomId",
+            sourceKey: "id",
+        });
+        db.User.hasMany(db.UserRoom);
+        db.UserRoom.belongsTo(db.User, {
+            foreignKey: "UserId",
+            sourceKey: "id",
+        });
     }
 };
