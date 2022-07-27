8a532181188ea3a453b8db6fea01932bc3f365b4
Hyomk <qet557@gmail.com>
2022-07-28 01:04:24 +0900

    fix : 모임방 나가기 라우터 수정

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,4 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 
-router.put("/:userId/room/:roomId", userController.deleteRoom);
-
 module.exports = router;
