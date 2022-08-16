e202ed5211165cea95219318b8ba4383b5aebcb7
최성현 <sunghyun@choeseonghyeon-ui-MacBookPro.local>
2022-08-16 23:50:21 +0900

    kakao front 적용

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const userRouter = require("./routes/userRoute");
 const roomRouter = require("./routes/roomRoute");
-const paymentRouter = require("./routes/paymentRoute")
+const paymentRouter = require("./routes/paymentRoute");
+const loginRouter = require("./routes/login");
+
+const bodyParser = require("body-parser");
+const nunjucks = require('nunjucks');
+const axios = require('axios');
+const qs = require('qs');
+const session = require('express-session');
 
 dotenv.config();
 const { sequelize } = require("./models");
 
 const app = express();
-app.set("port", process.env.PORT || 8001);
+app.set("port", process.env.PORT || 3000);
 
 sequelize
     .sync({ force: false })
@@ -21,6 +28,90 @@ sequelize
     .catch((err) => {
         console.error(err);
     });
+
+nunjucks.configure('views', {
+    express: app,
+})
+app.set('view engine', 'html')
+
+const kakao={
+    clientID : '187dfbd8517a2fdb721af0f630552827',
+    clientSecret : '08NwRRAOnO5kKs8pCdYsU953VMna3A3T',
+    redirectUri: 'http://localhost:3000/auth/kakao/callback'
+}
+
+app.get('/', (req,res)=>{
+    res.render('index.html');
+})
+
+app.use(session({
+    secret:'sung',
+    resave:true,
+    secure:false,
+    saveUninitialized:false,
+}));
+
+
+app.get('/auth/kakao', (req, res) => {
+    const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile_nickname,profile_image,account_email`;
+    res.redirect(kakaoAuthURL);
+});
+
+app.get('/auth/kakao/callback', async (req, res) => {
+    let token;
+    try {
+        token = await axios({
+            method: 'POST',
+            url: 'https://kauth.kakao.com/oauth/token',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            data: qs.stringify({
+                grant_type: 'authorization_code',
+                client_id: kakao.clientID,
+                client_secret: kakao.clientSecret,
+                redirectUri: kakao.redirectUri,
+                code: req.query.code,
+            })
+        })
+    } catch (err) {
+        res.json(err.data)
+    }
+
+
+
+    let user;
+    try{
+        user = await axios({
+            method:"GET",
+            url:'https://kapi.kakao.com/v2/user/me',
+            headers:{
+                Authorization:`Bearer ${token.data.access_token}`
+            }
+        })
+    }catch(err){
+        res.json(err.data);
+    }
+    console.log(user);
+
+
+
+    req.session.kakao = user.data;
+
+    res.redirect('/');
+
+});
+
+
+
+
+app.get('/auth/info',(req,res)=>{
+    let {nickname,profile_image} = req.session.kakao.properties
+    res.render('info.html',{
+        nickname,profile_image
+    });
+})
+
 
 app.use(morgan("dev"));
 app.use(cors());
