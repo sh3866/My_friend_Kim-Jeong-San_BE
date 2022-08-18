c647518600b72a6d78236744a8e3f3cc8eaea9a0
BaeGayeon <ebgy918@gmail.com>
2022-08-19 05:21:32 +0900

    feat: 정산 목록 불러오기

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -23,4 +23,18 @@ module.exports = {
             );
         }
     },
+    getRoomPayments: async function (req, res) {
+        try {
+            const roomId = req.params.roomId;
+            const paymentListResult = await paymentService.findRoomPayments(
+                roomId
+            );
+            res.status(200).send(
+                new ResponseDto(200, "정산 내역 조회 성공", paymentListResult)
+            );
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(new ResponseDto(400, "정산 내역 조회 실패"));
+        }
+    },
 };
